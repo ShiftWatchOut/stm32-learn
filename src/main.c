39a1e1b0393d2ab8415ae86f6062ef1e024ab0fe
shift <2080717464@qq.com>
2024-02-11 18:45:47 +0800

    ✨ feat: 另一种流水灯

--- src/main.c
+++ src/main.c
@@ -9,13 +9,18 @@ int main(void)
   GPIO_InitTypeDef gpioInit;
   gpioInit.Mode = GPIO_MODE_OUTPUT_PP;
   gpioInit.Pull = GPIO_PULLUP;
-  gpioInit.Pin = GPIO_PIN_0;
+  gpioInit.Pin = GPIO_PIN_All;
   gpioInit.Speed = GPIO_SPEED_HIGH;
   HAL_GPIO_Init(GPIOA, &gpioInit);
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_All, GPIO_PIN_SET);
   while (1)
   {
-    HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_0);
-    HAL_Delay(1000);
+    for (int i = 0; i < 7; i++)
+    {
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0 << i, GPIO_PIN_RESET);
+      HAL_Delay(300);
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0 << i, GPIO_PIN_SET);
+    }
   }
   return 0;
 }
