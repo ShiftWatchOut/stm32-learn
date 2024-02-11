8a0efefab729b16f798dd393dab6d1fd79b20518
shift <2080717464@qq.com>
2024-02-11 20:02:01 +0800

    ✨ feat: 红外传感器输入控制

--- src/main.c
+++ src/main.c
@@ -1,25 +1,44 @@
 #include "main.h"
 
+void LightSensor_Init(void)
+{
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  GPIO_InitTypeDef gpioInit;
+  gpioInit.Mode = GPIO_MODE_INPUT;
+  gpioInit.Pull = GPIO_PULLUP;
+  gpioInit.Pin = GPIO_PIN_14;
+  gpioInit.Speed = GPIO_SPEED_HIGH;
+  HAL_GPIO_Init(GPIOB, &gpioInit);
+}
+
+uint8_t LightSensor_Get(void)
+{
+  return HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_14);
+}
+
 int main(void)
 {
   HAL_Init();
   stm32_clock_init(RCC_PLL_MUL9);
+  LightSensor_Init();
 
   __HAL_RCC_GPIOA_CLK_ENABLE();
   GPIO_InitTypeDef gpioInit;
   gpioInit.Mode = GPIO_MODE_OUTPUT_PP;
   gpioInit.Pull = GPIO_PULLUP;
-  gpioInit.Pin = GPIO_PIN_All;
+  gpioInit.Pin = GPIO_PIN_0;
   gpioInit.Speed = GPIO_SPEED_HIGH;
   HAL_GPIO_Init(GPIOA, &gpioInit);
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_All, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_SET);
   while (1)
   {
-    for (int i = 0; i < 7; i++)
+    if (LightSensor_Get())
     {
-      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0 << i, GPIO_PIN_RESET);
-      HAL_Delay(300);
-      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0 << i, GPIO_PIN_SET);
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_RESET);
+    }
+    else
+    {
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_SET);
     }
   }
   return 0;
