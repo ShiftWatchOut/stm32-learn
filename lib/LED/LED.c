2b109c4bfe41d2ee2677de8a19f8651e9f28c699
shift <2080717464@qq.com>
2024-02-13 10:27:30 +0800

    ✨ feat: 允许点亮多个LED

--- lib/LED/LED.c
+++ lib/LED/LED.c
@@ -1,15 +1,17 @@
 #include "LED.h"
 
-/// @brief A0 处LED 初始化
+/// @brief 7个 LED 初始化
 void LED_A0_Init(void)
 {
+  __HAL_RCC_GPIOA_CLK_ENABLE();
   GPIO_InitTypeDef gpioInit;
   gpioInit.Mode = GPIO_MODE_OUTPUT_PP;
   gpioInit.Pull = GPIO_PULLUP;
-  gpioInit.Pin = GPIO_PIN_0;
+  uint32_t seven_pins = GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6;
+  gpioInit.Pin = seven_pins;
   gpioInit.Speed = GPIO_SPEED_HIGH;
   HAL_GPIO_Init(GPIOA, &gpioInit);
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(GPIOA, seven_pins, GPIO_PIN_SET);
 }
 
 /// @brief 红外传感器测试
@@ -23,4 +25,16 @@ void Toggle_Test(uint8_t val)
   {
     HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_SET);
   }
+}
+
+/// @brief 点亮指定 pin LED
+void LED_ON(uint16_t pin)
+{
+  HAL_GPIO_WritePin(GPIOA, pin, GPIO_PIN_RESET);
+}
+
+/// @brief 关闭指定 pin LED
+void LED_OFF(uint16_t pin)
+{
+  HAL_GPIO_WritePin(GPIOA, pin, GPIO_PIN_SET);
 }
--- lib/LED/LED.h
+++ lib/LED/LED.h
@@ -5,5 +5,7 @@
 
 void LED_A0_Init(void);
 void Toggle_Test(uint8_t val);
+void LED_ON(uint16_t pin);
+void LED_OFF(uint16_t pin);
 
 #endif // LED_H
